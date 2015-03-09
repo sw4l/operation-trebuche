381123c2420db86464bd8c1108a5bc529e020564
Scorch052 <misterhipster052@gmail.com>
2015-03-08 22:51:43 -0500

    Forgot Template mission
    Edited Userconfig

---
+++ Templates/TEI_Template.Stratis/init.sqf
@@ -0,0 +1,10 @@
+if ((!isServer) && (player != player)) then
+{
+waitUntil {player == player};
+};
+
+enableSaving [false, false];
+enableSentences false;
+setterraingrid 50;
+setviewdistance 2000;
+setobjectviewdistance 1000;
--- userconfig/TEI/TEI.hpp
+++ userconfig/TEI/TEI.hpp
@@ -5,4 +5,4 @@
 
 //Toggles the HUD Overlay that shows whenever the user wears an ODST helmet
 TEI_HUD_Enabled = true; //Values are true and false
-TEI_HUD_Color = "TEI_ODST_HUD_blue"; //Values are TEI_ODST_HUD_blue / TEI_ODST_HUD_orange / TEI_ODST_HUD_white / TEI_ODST_HUD_green / TEI_ODST_HUD_red / TEI_ODST_HUD_purple
+TEI_HUD_Color = "TEI_ODST_HUD_blue"; //Values are TEI_ODST_HUD_blue / TEI_ODST_HUD_orange / TEI_ODST_HUD_white / TEI_ODST_HUD_green / TEI_ODST_HUD_purple
