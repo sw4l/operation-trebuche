9278ffc0bc6120afec72bbbdfb8717312cd0c23f
TheDog88 <crmthedog@yahoo.com>
2015-03-10 17:41:36 -0400

    added TEI_frigate (DO NOT INCLUDE THIS IN ANY BUILDS UNTIL ITS DONE)

---
+++ TEI_Frigate/config.cpp
@@ -0,0 +1,41 @@
+
+class CfgPatches
+{
+	class TEI_Ins_Object_class
+	{
+		units[]										= {};
+		weapons[]									= {};
+		requiredVersion								= 0.1;
+		requiredAddons[]							= {"TEI_Core"};
+	};
+};
+
+class CfgVehicles
+{
+	class All {};
+	class Static: All {};
+	class Building: Static {};
+	class NonStrategic: Building {};
+	class TargetTraining: NonStrategic {};
+	class TargetGrenade: TargetTraining {};
+	
+	
+	class TEI_testbox: static
+	{
+		scope = 2;
+		vehicleClass = "TEI_UNSC_Object_class";
+		displayName = "test box";
+		model = "\TEI_frigate\testbox.p3d";
+		author = "Eridanus Insurrection Team";
+		icon = "iconCrateWpns";
+	};
+	class TEI_final_dawn: static
+	{
+		scope = 2;
+		vehicleClass = "TEI_UNSC_Object_class";
+		displayName = "Final Dawn";
+		model = "\TEI_frigate\finaldawn.p3d";
+		author = "Eridanus Insurrection Team";
+		icon = "iconCrateWpns";
+	};
+};
