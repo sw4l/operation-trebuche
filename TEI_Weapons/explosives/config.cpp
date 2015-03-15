42dd7f11b6b935b0b1aa7097e83469bba91648b5
spacenavy90 <spacenavy90@gmail.com>
2015-03-15 16:30:03 -0500

    [Added] M9 HEDP Frag

--- TEI_Weapons/explosives/config.cpp
+++ TEI_Weapons/explosives/config.cpp
@@ -15,27 +15,54 @@ class CfgPatches {
         };
 };
 
+class cfgWeapons
+{
+	class Default;
+	class GrenadeLauncher;
+	
+	class Throw: GrenadeLauncher
+	{
+		class ThrowMuzzle;
+		 muzzles[] += {"TEI_M9FragMuzzle"};	
+			
+		class TEI_M9FragMuzzle: ThrowMuzzle
+		{
+			magazines[] = {"TEI_M9_Frag"};
+		};
+	};
+};
+
 class CfgMagazines
 {
 	class HandGrenade;
 	class CA_Magazine;
-	
-	/*class TEI_M9_Frag: CA_Magazine //TEI_M9_Frag needs to be added to a custom throwing weapon for UNSC units or the base vanilla A3 weapon "throw". Idk if we can change that though.
+
+	class TEI_M9_Frag: HandGrenade
 	{
+		ammo = "TEI_G_M9_Frag";
 		mass = 10;
-		scope = 0;
+		scope = 2;
 		value = 1;
 		displayName = "[UNSC] M9 HE-DP Grenade";
+		descriptionShort = "Type: Defensive Grenade<br />Rounds: 1<br />Used in: Hand";
+		displayNameShort = "M9 Frag";
+		model = "\TEI_Weapons\explosives\m9_grenade.p3d";
 		picture = "\TEI_Weapons\explosives\icons\m9.paa";
-		//model = "\TEI_Weapons\explosives\m9_grenade.p3d";
 		type = 256;
-		ammo = "GrenadeHand";
 		count = 1;
 		initSpeed = 18;
 		nameSound = "handgrenade";
 		maxLeadSpeed = 7;
 		sound[] = {"",0.000316228,1};
-		descriptionShort = "Type: Defensive Grenade<br />Rounds: 1<br />Used in: Hand";
-		displayNameShort = "M9 Frag";
-	};*/
+	};
+};
+
+class cfgAmmo
+{
+	class GrenadeHand;
+	
+	class TEI_G_M9_Frag: GrenadeHand
+	{
+		model = "\TEI_Weapons\explosives\m9_grenade.p3d";
+	};
 };
