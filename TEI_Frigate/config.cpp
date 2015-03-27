a39f19c9feb9df57475419158104ef361ee9d67c
TheDog88 <crmthedog@yahoo.com>
2015-03-26 23:18:49 -0400

    gun turret is operation, still need to fix alternating barrels and create legit weapon source for it (currently using 120mm)

--- TEI_Frigate/config.cpp
+++ TEI_Frigate/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches
 		units[]										= {};
 		weapons[]									= {};
 		requiredVersion								= 0.1;
-		requiredAddons[]							= {"TEI_Core"};
+		requiredAddons[]							= {"TEI_Core","static_f"};
 	};
 };
 
@@ -18,12 +18,257 @@ class CfgVehicles
 	class NonStrategic: Building {};
 	class TargetTraining: NonStrategic {};
 	class TargetGrenade: TargetTraining {};
-	class LandVehicle;
-	class StaticWeapon:LandVehicle
+	class Strategic;
+	class Land;
+	class LandVehicle: Land
 	{
+		class ViewPilot;
 		class NewTurret;
 	};
-	
+	class StaticWeapon: LandVehicle
+	{
+		epeImpulseDamageCoef = 8.0;
+		vehicleClass = "Static";
+		driverOpticsModel = "\A3\weapons_f\reticle\optics_empty";
+		selectionClan = "clan";
+		selectionShowDamage = "poskozeni";
+		bounding = "usti hlavne";
+		alphaTracks = 0.7;
+		textureTrackWheel = 0;
+		unitInfoType = "RscUnitInfoSoldier";
+		waterLeakiness = 100;
+		selectionFireAnim = "";
+		fireDustEffect = "FDustEffects";
+		class DestructionEffects{};
+		memoryPointMissile[] = {"spice rakety","usti hlavne"};
+		memoryPointMissileDir[] = {"konec rakety","konec hlavne"};
+		memoryPointCargoLight = "cargo light";
+		gunnerCanSee = "2+8+32+4";
+		gunnerHasFlares = 0;
+		class Turrets
+		{
+			class MainTurret: NewTurret
+			{
+				selectionFireAnim = "";
+				memoryPointsGetInGunner = "pos_gunner_dir";
+				memoryPointsGetInGunnerDir = "pos_gunner";
+				memoryPointGun = "usti hlavne";
+				gunbeg = "usti hlavne";
+				gunend = "konec hlavne";
+				outGunnerMayFire = 1;
+				optics = 1;
+				minTurn = -40;
+				maxTurn = 40;
+				initTurn = 0;
+				minElev = -7;
+				maxElev = 70;
+				initElev = 0;
+				castGunnerShadow = 1;
+				ejectDeadGunner = 0;
+				canEject = 0;
+				gunnerGetInAction = "";
+				gunnerGetOutAction = "";
+				gunnerForceOptics = 0;
+				class ViewOptics
+				{
+					initAngleX = 0;
+					minAngleX = -30;
+					maxAngleX = 30;
+					initAngleY = 0;
+					minAngleY = -100;
+					maxAngleY = 100;
+					initFov = 0.25;
+					minFov = 0.10;
+					maxFov = 1.1;
+					minMoveX = 0;
+					maxMoveX = 0;
+					minMoveY = 0;
+					maxMoveY = 0;
+					minMoveZ = 0;
+					maxMoveZ = 0;
+				};
+				class HitPoints
+				{
+					class HitGun
+					{
+						armor = 0.3;
+						material = -1;
+						name = "action";
+						visual = "autonomous_unhide";
+						passThrough = 0;
+						radius = 0.2;
+					};
+					class HitTurret: HitGun
+					{
+						armor = 0.3;
+						class DestructionEffects
+						{
+							class Smoke
+							{
+								simulation = "particles";
+								type = "WeaponWreckSmoke";
+								position = "destructionEffect";
+								intensity = 1;
+								interval = 1;
+								lifeTime = 5;
+							};
+						};
+					};
+				};
+			};
+		};
+		damperSize = 1.4;
+		damperForce = 0.8;
+		gearBox[] = {-1,0,1};
+		attenuationEffectType = "OpenCarAttenuation";
+		soundEnviron[] = {"",1.0,0.7};
+		soundEngine[] = {"",10.0,1};
+		soundCrash[] = {"",0.56234133,1};
+		soundGear[] = {"",0.00031622776,1};
+		soundDammage[] = {"",0.01,1};
+		getInAction = "";
+		getOutAction = "";
+		cargoGetInAction[] = {};
+		cargoGetOutAction[] = {};
+		class Wounds
+		{
+			tex[] = {};
+			mat[] = {};
+		};
+		mapSize = 2.55;
+		getInRadius = 3.5;
+		secondaryExplosion = -1;
+		fuelExplosionPower = 0;
+		fuelCapacity = 0;
+		irScanRangeMin = 500;
+		irScanRangeMax = 4000;
+		irScanToEyeFactor = 1;
+		irTarget = 0;
+		laserTarget = 0;
+		armor = 24;
+		armorStructural = 1;
+		explosionShielding = 1;
+		minTotalDamageThreshold = 0.01;
+		crewCrashProtection = 1;
+		crewExplosionProtection = 0;
+		class HitPoints
+		{
+			class HitEngine
+			{
+				armor = 0.8;
+				material = -1;
+				name = "motor";
+				passThrough = 1;
+			};
+			class HitHull
+			{
+				armor = 1;
+				material = -1;
+				name = "telo";
+				passThrough = 1;
+			};
+			class HitTurret
+			{
+				armor = 0.8;
+				material = -1;
+				name = "vez";
+				passThrough = 1;
+			};
+			class HitGun
+			{
+				armor = 0.6;
+				material = -1;
+				name = "zbran";
+				passThrough = 1;
+			};
+			class HitLTrack
+			{
+				armor = 0.6;
+				material = -1;
+				name = "pas_L";
+				passThrough = 1;
+			};
+			class HitRTrack
+			{
+				armor = 0.6;
+				material = -1;
+				name = "pas_P";
+				passThrough = 1;
+			};
+			class HitBody
+			{
+				armor = 1;
+				material = -1;
+				name = "NEzbytek";
+				visual = "zbytek";
+				passThrough = 1;
+			};
+		};
+		extCameraPosition[] = {0,1.5,-9};
+		class ViewPilot: ViewPilot
+		{
+			initAngleX = 7;
+			minAngleX = -15;
+			maxAngleX = 25;
+			initAngleY = 0;
+			minAngleY = -90;
+			maxAngleY = 90;
+		};
+		cost = 10000;
+		steerAheadSimul = 0.4;
+		steerAheadPlan = 0.6;
+		predictTurnSimul = 1.2;
+		predictTurnPlan = 1.8;
+		ejectDeadDriver = 1;
+		ejectDeadCargo = 1;
+		hasDriver = 0;
+		nightVision = 0;
+		driverAction = "ManActCargo";
+		driverInAction = "ManActCargo";
+		memoryPointsGetInCargo = "pos_cargo";
+		memoryPointsGetInCargoDir = "pos_cargo_dir";
+		simulation = "tankX";
+		occludeSoundsWhenIn = 1.0;
+		obstructSoundsWhenIn = 1.0;
+		formationX = 20;
+		formationZ = 30;
+		precision = 1;
+		brakeDistance = 0;
+		turnCoef = 0;
+		maxSpeed = 0;
+		canFloat = 0;
+		weapons[] = {"FakeWeapon"};
+		magazines[] = {"FakeWeapon"};
+		type = 1;
+		threat[] = {0.7,1,0.3};
+		camouflage = 1;
+		audible = 1;
+		hideProxyInCombat = 0;
+		driverOpticsColor[] = {0,0,0,1};
+		class CargoLight
+		{
+			color[] = {0,0,0,0};
+			ambient[] = {0.6,0,0.15,1};
+			brightness = 0.007;
+		};
+		transportMaxMagazines = 0;
+		transportMaxWeapons = 0;
+		class Reflectors{};
+		class UserActions
+		{
+			class PressXToFlipTheThing
+			{
+				displayNameDefault = "$STR_A3_StaticWeapon_UserActions_PressXToFlipTheThing0";
+				displayName = "$STR_A3_StaticWeapon_UserActions_PressXToFlipTheThing0";
+				position = "";
+				radius = 2.7;
+				onlyForPlayer = 1;
+				condition = "alive this AND not canmove this AND count crew this == 0";
+				statement = "this setpos [getpos this select 0,getpos this select 1,(getpos this select 2)+1]";
+			};
+		};
+		numberPhysicalWheels = 0;
+	};
 	
 	class TEI_testbox: static
 	{
@@ -366,11 +611,11 @@ class CfgVehicles
 				gun="maingun";
 				animationsourcebody="mainturret";
 				animationSourceGun="maingun";
-				gunAxis="Osa Hlavne";
+				gunAxis="OsaHlavne";
 				gunBeg="Usti hlavne";
 				gunEnd="konec hlavne";
-				minElev=-8;
-				maxElev=16;
+				minElev=-4;
+				maxElev=50;
 				minTurn=-360;
 				maxTurn=360;
 				initTurn=0;
@@ -382,33 +627,30 @@ class CfgVehicles
 				gunnerGetInAction="GetInMRAP_01";
 				gunnerGetOutAction="GetOutMRAP_01";
 				gunnerName="Gunner";
-				hideWeaponsGunner=0;
-				soundServo[]={"",0.01,1};
 				outGunnerMayFire=1;
 				inGunnerMayFire=1;
 				commanding=1;
 				primaryGunner=1;
+				soundServo[] = { "", 0.01, 2.000000 };
 				memoryPointsGetInGunner="Pos Gunner";
 				memoryPointsGetInGunnerDir="Pos Gunner dir";
 				gunnerLeftHandAnimName="trigger";
 				gunnerRightHandAnimName="trigger";
 				memoryPointGun="machinegun";
-				weapons[]={"TEI_102R_Turret"};
-				magazines[] = {"3Rnd_102mm_rockets","3Rnd_102mm_rockets"};
+				weapons[]={"cannon_120mm"};
+				magazines[] = {"32Rnd_120mm_APFSDS_shells_Tracer_Red","32Rnd_120mm_APFSDS_shells_Tracer_Red","32Rnd_120mm_APFSDS_shells_Tracer_Red"};
 				memoryPointGunnerOptics="gunnerview";
 				gunnerOpticsModel = "\A3\Weapons_F\Reticle\Optics_Gunner_MBT_01_w_F.p3d";
-				//gunnerOpticsModel = "\A3\weapons_f\reticle\Optics_Gunner_02_F";
-				//gunnerOpticsModel="a3\weapons_f\Reticle\optics_empty";
 				gunnerOpticsShowCursor=1;
 				turretInfoType="RscWeaponZeroing";
-				//turretInfoType="RscOptics_crows";
 				visionMode[] = {"Normal","Ti"};
 				thermalMode[] = {2};
 				castGunnerShadow=1;
 				startEngine=0;
 				enableManualFire=0;
-
-				gunnerForceOptics=0;
+				gunnerForceOptics = 1;
+				gunnerForceOutOptics = 1;
+				hideWeaponsGunner = 1;
 				class ViewGunner:ViewOptics
 				{
 					initFov=0.7;
@@ -417,5 +659,5 @@ class CfgVehicles
 				};
 			};
 		};
-	
+	};
 };
