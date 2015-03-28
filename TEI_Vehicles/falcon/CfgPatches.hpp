4f9627fa5d2db6695e938d581206fa371c6319de
TheDog88 <crmthedog@yahoo.com>
2015-03-28 00:26:05 -0400

    functional falcon. needs a shit ton of work

---
+++ TEI_Vehicles/falcon/CfgPatches.hpp
@@ -0,0 +1,10 @@
+class CfgPatches
+{
+	class TEI_Vehicles_Falcon
+	{
+		units[] = {"TEI_Pelican_unarmed"};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Air_F","A3_Air_F_Beta","A3_Weapons_F","TEI_Core"};
+	};
+};
---
+++ TEI_Vehicles/falcon/rtd.hpp
@@ -0,0 +1,18 @@
+﻿		class RotorLibHelicopterProperties: RotorLibHelicopterProperties
+		{
+			RTDconfig = "Samples_F\Test_Heli_01\RTD_Test_Heli_01.xml";
+			defaultCollective 				= 0.545;
+			autoHoverCorrection[] 			= {4.8,-3.2,0};	// pitch, roll, no
+			retreatBladeStallWarningSpeed 	= 83.333 ; 		// m/s
+
+			maxTorque 			= 2700 ; 				// 90% of both engines torque in XML
+			stressDamagePerSec	= 0.0033333332666667; 	// after 300 seconds the damage will be 1
+						
+			maxHorizontalStabilizerLeftStress	= 10000;
+			maxHorizontalStabilizerRightStress	= 10000;
+			maxVerticalStabilizerStress			= 10000;
+			horizontalWingsAngleCollMin			= 0;
+			horizontalWingsAngleCollMax			= 0;
+			maxMainRotorStress 					= 130000;
+			maxTailRotorStress 					= 10000;
+		};
