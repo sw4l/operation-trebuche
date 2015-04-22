3540c5e6ffdb7d8d4c96fc41d7798173fd8f28ff
Scorch052 <misterhipster052@gmail.com>
2015-04-22 05:43:47 -0500

    [Removed] Outdated Templates (will add new ones soon)
    [Uploaded] Burgess' Retextures (will apply soon)

--- Release Staging Area/userconfig/OPTRE/OPTRE.hpp
+++
@@ -1,8 +0,0 @@
-/* 	
-	Operation: TREBUCHET Userconfig Files
-	Change the variables within this file in order to modify the mod to your liking
-*/
-
-//Toggles the HUD Overlay that shows whenever the user wears an ODST helmet
-OPTRE_HUD_Enabled = true; //Values are true and false
-OPTRE_HUD_Color = "OPTRE_ODST_HUD_blue"; //Values are OPTRE_ODST_HUD_blue / OPTRE_ODST_HUD_orange / OPTRE_ODST_HUD_white / OPTRE_ODST_HUD_green / OPTRE_ODST_HUD_purple
