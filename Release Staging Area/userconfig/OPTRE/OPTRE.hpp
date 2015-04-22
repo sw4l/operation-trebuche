d7c1db2105f6a5fad004cc3c11812902a90cef3f
Scorch052 <misterhipster052@gmail.com>
2015-04-22 03:34:22 -0500

    [Added] Release Staging Area Folder (Folder to be packed and released)
    Please try to remember to update the classlist and changelog as you make progress on various aspects of the mod.
    
    Thanks

--- Release Staging Area/userconfig/OPTRE/OPTRE.hpp
+++ Release Staging Area/userconfig/OPTRE/OPTRE.hpp
@@ -5,4 +5,4 @@
 
 //Toggles the HUD Overlay that shows whenever the user wears an ODST helmet
 OPTRE_HUD_Enabled = true; //Values are true and false
-OPTRE_HUD_Color = "TEI_ODST_HUD_blue"; //Values are OPTRE_ODST_HUD_blue / OPTRE_ODST_HUD_orange / OPTRE_ODST_HUD_white / OPTRE_ODST_HUD_green / OPTRE_ODST_HUD_purple
+OPTRE_HUD_Color = "OPTRE_ODST_HUD_blue"; //Values are OPTRE_ODST_HUD_blue / OPTRE_ODST_HUD_orange / OPTRE_ODST_HUD_white / OPTRE_ODST_HUD_green / OPTRE_ODST_HUD_purple
