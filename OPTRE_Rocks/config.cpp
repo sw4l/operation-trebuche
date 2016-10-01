e134ce1b772701cd8f74a6b92b471c5a1e207d26
thedog88 <crmthedog@yahoo.com>
2016-10-01 00:44:14 -0400

    added optre rocks
    includes 11 different iced rock shapes in 2 variants each for a total of
    22 iced rock objects. we need to create a proper subcategory for this as
    well as secundus hallways in optre_core.

---
+++ OPTRE_Rocks/config.cpp
@@ -0,0 +1,347 @@
+class CfgPatches
+{
+	
+	class OPTRE_UNSC_Structure_Highway
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = {"A3_structures_f","OPTRE_Core"};
+    };
+};
+
+class CfgVehicles
+{
+	class House_F;
+	
+	class Optre_Rocks_ICE_Rock_Boulder_01: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock Boulder 01";
+		model = "\OPTRE_Rocks\ICE_Rock_Boulder_01.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_Rock_Boulder_02: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock Boulder 02";
+		model = "\OPTRE_Rocks\ICE_Rock_Boulder_02.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_Rock_Boulder_03: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock Boulder 03";
+		model = "\OPTRE_Rocks\ICE_Rock_Boulder_03.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_Rock_Boulder_04: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock Boulder 04";
+		model = "\OPTRE_Rocks\ICE_Rock_Boulder_05.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_Rock_Boulder_05: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock Boulder 05";
+		model = "\OPTRE_Rocks\ICE_Rock_Boulder_06.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_Rock_Rock_01: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock Rock 01";
+		model = "\OPTRE_Rocks\ICE_Rock_Rock_01.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_Rock_Rock_02: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock Rock 02";
+		model = "\OPTRE_Rocks\ICE_Rock_Rock_02.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_Rock_Rock_03: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock Rock 03";
+		model = "\OPTRE_Rocks\ICE_Rock_Rock_03.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_Rock_Rocktower: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock tower";
+		model = "\OPTRE_Rocks\ICE_Rock_Rocktower.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_Rock_RockWall: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock Wall";
+		model = "\OPTRE_Rocks\ICE_Rock_RockWall.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_Rock_stone: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Rock Stone";
+		model = "\OPTRE_Rocks\ICE_Rock_stone_03.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+class Optre_Rocks_ICE_ICE_Boulder_01: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice Boulder 01";
+		model = "\OPTRE_Rocks\ICE_ICE_Boulder_01.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_ICE_Boulder_02: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice Boulder 02";
+		model = "\OPTRE_Rocks\ICE_ICE_Boulder_02.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_ICE_Boulder_03: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice Boulder 03";
+		model = "\OPTRE_Rocks\ICE_ICE_Boulder_03.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_ICE_Boulder_04: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice Boulder 04";
+		model = "\OPTRE_Rocks\ICE_ICE_Boulder_05.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_ICE_Boulder_05: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice Boulder 05";
+		model = "\OPTRE_Rocks\ICE_ICE_Boulder_06.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_ICE_Rock_01: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice Rock 01";
+		model = "\OPTRE_Rocks\ICE_ICE_Rock_01.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_ICE_Rock_02: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice Rock 02";
+		model = "\OPTRE_Rocks\ICE_ICE_Rock_02.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_ICE_Rock_03: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice Rock 03";
+		model = "\OPTRE_Rocks\ICE_ICE_Rock_03.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_ICE_Rocktower: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice tower";
+		model = "\OPTRE_Rocks\ICE_ICE_Rocktower.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_ICE_RockWall: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice Wall";
+		model = "\OPTRE_Rocks\ICE_ICE_RockWall.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+	class Optre_Rocks_ICE_ICE_stone: House_F
+	{
+		dlc = "OPTRE";
+		scope = 2;
+		scopeCurator = 2;
+		armor = 999999;
+		armorStructural = 999;
+		vehicleClass = "OPTRE_Trans_Building_class";
+		displayName = "Ice Ice Stone";
+		model = "\OPTRE_Rocks\ICE_ICE_stone_03.p3d";
+		author = "Article 2 Studios";
+		icon = "iconCrateWpns";
+		editorCategory = "OPTRE_EditorCategory_Buildings";
+		editorSubcategory = "OPTRE_EditorSubcategory_Buildings_Highway";
+	};
+};
